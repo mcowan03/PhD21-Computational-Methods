a8332e812391fa468d46b8cd283fb7e4c83bfe4e
mcowan03 <maxcowan03@gmail.com>
2025-11-25 10:27:04 +0000

    ReadME
    Added work in progress notes and context for V1.

---
+++ Replications/Hopenhayn/README.m
@@ -0,0 +1,5 @@
+%% Work in Progress %%
+
+% V1 is Hopenhayn 1992 - abstracts from household, and follows Chris Edmond and Tomasrm notes.
+  % V1 is partial equilibrium, as HH side demand is exogenous. Endogenous firm entry and exit. We find a stationary equilibrium with a positive mass of entrants, such that Free Entry Condition holds.
+  % Will look to extend...
